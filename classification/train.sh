8e84d2b2c960e7b27cf4c59e0b5bb8933c0c6782
NICHOLAS-1217 <nicholastan1217@gmail.com>
2025-06-01 13:45:23 +0800

    first commit

---
+++ classification/train.sh
@@ -0,0 +1,39 @@
+torchrun \
+   --nproc_per_node=1 \
+   --master_port=12340 \
+   main.py \
+   --cfg configs/vssm/vmambav2v_tiny_224.yaml \
+   --data-path /home/nicho/documents/cassava \
+   --batch-size 16 \
+   --output out/
+
+torchrun \
+   --nproc_per_node=1 \
+   --master_port=12340 \
+   main.py \
+   --cfg configs/vssm/vmambav2_small_224.yaml\
+   --data-path /home/nicho/documents/cassava \
+   --batch-size 16 \
+   --output out/
+
+torchrun \
+   --nproc_per_node=1 \
+   --master_port=12340 \
+   main.py \
+   --cfg configs/vssm/vmambav2_base_224.yaml\
+   --data-path /home/nicho/documents/cassava \
+   --batch-size 16 \
+   --early_stopping_patience 30 \
+   --early_stopping_min_delta 0.001 \
+   --output out/
+
+torchrun \
+   --nproc_per_node=1 \
+   --master_port=12340 \
+   main.py \
+   --cfg configs/vssm/vmambav2_base_224.yaml\
+   --data-path /home/nicho/documents/cassava \
+   --batch-size 16 \
+   --early_stopping_patience 100 \
+   --early_stopping_min_delta 0.001 \
+   --output out/
